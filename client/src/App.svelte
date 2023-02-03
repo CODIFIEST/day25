03f0a467b49e133a4d1d4a1b8de95b956feca89c
CODIFIEST <surfdean808@gmail.com>
2023-02-02 14:15:34 -1000

    first commit from day24 data

---
+++ client/src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import VideoGameForm from "../components/VideoGameForm.svelte";
+    import VideoGameList from "../components/VideoGameList.svelte";
+    let viewForm:boolean = true
+    function toggleDisplay() {
+  viewForm = !viewForm;
+}
+
+</script>
+
+<main>
+  <button on:click={toggleDisplay}>Toggle the Display</button>
+  {#if viewForm}
+ <!-- <UserForm /> -->
+ <VideoGameForm />
+ {:else}
+ <VideoGameList />
+ <!-- <UserList /> -->
+ {/if}
+</main>
---
+++ server/domain/game.ts
@@ -0,0 +1,10 @@
+type Game ={
+    name:string,
+    platform:string,
+    releaseYear:number,
+    genre:string,
+    ESRBrating:string,
+    goodGame:boolean,
+
+}
+export type {Game}
